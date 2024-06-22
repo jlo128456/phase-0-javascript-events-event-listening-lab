7874a974bc78c4eab6a78dda8a5b9bb70dec10e3
Jeffrey <j.lo128456@gmail.com>
2024-06-23 07:50:42 +1000

    completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert(){
+        alert('I was Clicked!');
+    }
+    input.addEventListener('click',clickAlert);
 }
+addingEventListener();
+
